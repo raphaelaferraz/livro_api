d2b2f8c1b5a29473699914cb97182349d18bcf79
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-02 14:32:43 -0300

    Adição dos serviços do AutoMapper no arquivo principal

--- livro_api/Program.cs
+++ livro_api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adicionando os serviços do AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
