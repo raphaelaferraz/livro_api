ed5709eabe2d0ac0a549f428fa804aa7aaaaf3fb
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-02 13:59:14 -0300

    Adição de comentários na classe principal Editora

--- livro_api/Models/Editora.cs
+++ livro_api/Models/Editora.cs
@@ -4,10 +4,12 @@ namespace livro_api.Models;
 
 public class Editora
 {
+    // Propriedade obrigatória que define o Id da editora
     [Key]
     [Required]
     public int Id { get; set; }
 
+    // Propriedade obrigatória que define o nome da editora
     [Required(ErrorMessage = "O nome da editora é obrigatório")]
     public string Nome { get; set; }
 }
