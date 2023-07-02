84da3e5a37b7bacd8b8c4538c6a2c461ddc54c49
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-02 14:02:30 -0300

    Adição de comentários na classe principal Livro

--- livro_api/Models/Livro.cs
+++ livro_api/Models/Livro.cs
@@ -4,16 +4,20 @@ namespace livro_api.Models;
 
 public class Livro
 {
+    // Propriedade obrigatória que define o Id do livro
     [Key]
     [Required]
     public int Id { get; set; }
 
+    // Propriedade obrigatória que define o título do livro
     [Required(ErrorMessage = "O título do livro é obrigatório")]
     public string Titulo { get; set; }
 
+    // Propriedade obrigatória que define a quantidade de páginas do livro
     [Required(ErrorMessage = "A quantidade de páginas do livro é obrigatória")]
     public int QuantidadeDePaginas { get; set; }
 
+    // Propriedade obrigatória que define o idioma do livro
     [Required(ErrorMessage = "O idioma do livro é obrigatório")]
     public string Idioma { get; set; }
 }
