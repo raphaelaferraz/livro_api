aa5f9ff411d8a17ab4d7f4519e8bb1c6767269ed
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-02 13:58:22 -0300

    Criação da classe principal Editora

---
+++ livro_api/Models/Editora.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace livro_api.Models;
+
+public class Editora
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome da editora é obrigatório")]
+    public string Nome { get; set; }
+}
