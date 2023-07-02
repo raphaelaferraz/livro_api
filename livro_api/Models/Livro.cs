0f14596d34763ec5c4bf68e3ecd4f003f93acde1
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-02 14:01:36 -0300

    Criação da classe principal Livro

---
+++ livro_api/Models/Livro.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace livro_api.Models;
+
+public class Livro
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O título do livro é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "A quantidade de páginas do livro é obrigatória")]
+    public int QuantidadeDePaginas { get; set; }
+
+    [Required(ErrorMessage = "O idioma do livro é obrigatório")]
+    public string Idioma { get; set; }
+}
