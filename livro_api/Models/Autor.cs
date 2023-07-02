7da463357e32bb524b42182ebd9000f0a326d861
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-02 13:56:01 -0300

    Criação da classe principal Autor

---
+++ livro_api/Models/Autor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace livro_api.Models;
+
+public class Autor
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome do autor é obrigatório")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A biografia do autor é obrigatória")]
+    [StringLength(250, ErrorMessage = "A biografia pode ter apenas 250 caracteres")]
+    public string Biografia { get; set; }
+}
