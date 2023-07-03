8567340137859fa0e8d8aeb2fa936d04d8cf7400
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-03 07:40:05 -0300

    Adição de comentários na classe profile Livro

--- livro_api/Profiles/LivroProfile.cs
+++ livro_api/Profiles/LivroProfile.cs
@@ -8,10 +8,13 @@ public class LivroProfile : Profile
 {
     public LivroProfile()
     {
+        // Criação do mapeamento da classe básica de criação para a classe principal Livro
         CreateMap<CreateLivroDto, Livro>();
 
+        // Criação do mapeamento da classe principal Livro para a classe de leitura 
         CreateMap<Livro, ReadLivroDto>();
 
+        // Criação do mapeamento da classe básica de atualização para a classe principal Livro
         CreateMap<UpdateLivroDto, Livro>();
     }
 }
