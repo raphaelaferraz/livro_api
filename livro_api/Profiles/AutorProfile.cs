081528adf57b98986567078fe78b80c508b46d32
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-03 07:34:09 -0300

    Adição das propriedades na classe profile do Autor

--- livro_api/Profiles/AutorProfile.cs
+++ livro_api/Profiles/AutorProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using livro_api.Data.Dtos;
+using livro_api.Models;
 
 namespace livro_api.Profiles;
 
@@ -6,5 +8,10 @@ public class AutorProfile : Profile
 {
     public AutorProfile()
     {
+        CreateMap<CreateAutorDto, Autor>();
+
+        CreateMap<Autor, ReadAutorDto>();
+
+        CreateMap<UpdateAutorDto, Autor>();
     }
 }
