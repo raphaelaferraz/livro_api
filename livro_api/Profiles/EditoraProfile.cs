8d6e37ff831cb496953704ce4b9e19724d6eff8f
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-03 07:42:01 -0300

    Adição de comentários na classe profile Editora

--- livro_api/Profiles/EditoraProfile.cs
+++ livro_api/Profiles/EditoraProfile.cs
@@ -8,10 +8,13 @@ public class EditoraProfile : Profile
 {
     public EditoraProfile()
     {
+        // Criação do mapeamento da classe básica de criação para a classe principal Editora
         CreateMap<CreateEditoraDto, Editora>();
 
+        // Criação do mapeamento da classe principal Editora para a classe básica de leitura
         CreateMap<Editora, ReadEditoraDto>();
 
+        // Criação do mapeamento da classe básica de atualização para a classe principal Editora
         CreateMap<UpdateEditoraDto, Editora>();
     }
 }
