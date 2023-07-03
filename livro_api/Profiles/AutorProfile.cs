6093a08623dbf68a4b93e185d9a29c68df0d1764
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-03 07:42:57 -0300

    Adição de comentários na classe profile Autor

--- livro_api/Profiles/AutorProfile.cs
+++ livro_api/Profiles/AutorProfile.cs
@@ -8,10 +8,13 @@ public class AutorProfile : Profile
 {
     public AutorProfile()
     {
+        // Criação do mapeamento da classe básica de criação para a classe principal Autor
         CreateMap<CreateAutorDto, Autor>();
 
+        // Criação do mapeamento da classe principal Autor para a classe básica de leitura 
         CreateMap<Autor, ReadAutorDto>();
 
+        // Criação do mapeamento da classe básica de atualização para a classe principal Autor
         CreateMap<UpdateAutorDto, Autor>();
     }
 }
